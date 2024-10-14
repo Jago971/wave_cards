6117efc1b7cc8c394fb569c8bcbf07297bb9d3e6
Matt_Mannings <matt.mannings97@gmail.com>
2024-10-14 11:34:07 +0100

    added color change

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 "use strict";
+let color = 'blue';
 
 const cards = document.querySelectorAll(".card");
 const container = document.querySelector(".container");
 
 container.style.animation = 'shuffle 2s infinite';
-container.style.animationDelay = '-1.75s'
+container.style.animationDelay = '-1.75s';
 
 for(let i = 0; i < cards.length; i++) {
   cards[i].style.animation = 'wave 2s infinite'
@@ -26,3 +27,19 @@ container.addEventListener("mouseout", (e) => {
   container.style.animation = 'shuffle 2s infinite';
   container.tyle.animationDelay = '-1.75s'
 })
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    if(color == 'blue') {
+      document.documentElement.style.setProperty('--bright', 'red');
+      document.documentElement.style.setProperty('--dark', 'rgb(100, 0, 0');
+      document.documentElement.style.setProperty('--darker', 'rgb(50, 0, 0');
+      color = 'red'
+    } else {
+      document.documentElement.style.setProperty('--bright', 'aqua');
+      document.documentElement.style.setProperty('--dark', 'rgb(0, 100, 100');
+      document.documentElement.style.setProperty('--darker', 'rgb(0, 50, 50');
+      color = 'blue'
+    }
+  })
+});
